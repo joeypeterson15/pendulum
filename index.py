aadd9db6d7f7383ae08a9fbecbb4802cc7c483f1
Joey Peterson <joey.peterson15@gmail.com>
2024-10-03 16:34:30 -0700

    pendulum class and update loop

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from graphics import *
+from sympy import symbols, diff
 import numpy 
 # Pendulum: equation:   
     # torque = τ = I(α) where I = moment of Intertia = mL^2
@@ -8,18 +9,33 @@ import numpy
     #       d^2(θ) / d(t)^2 + (g/L)sin(θ) = 0
 g = 9.8
 pi = 3.14
-class pendulum:
-    def __init__(self, initAngle, mass, length, frequency):
-        self.angle = initAngle
-        self.mass = mass
-        self.length = length
-        self.angFreq = frequency * 2 * pi
-        self.angle = 
-        # φ = arctan(ω₀ / (θ₀ √(g/L)))
-        self.offset =  numpy.arctan((self.angFreq / initAngle) * numpy.sqrt(g / length))
+frameTime = 0
+xShift = 200
+yShift = 200
 
-    def process(self): # need to update the angle every time we get called
-        self.angle += self.offset
+class Pendulum:
+    def __init__(self, initAngle, length):
+        self.angle = initAngle
+        self.length = length
+        self.oscillationConst = g / length# not to be confused with angular velocity. 
+        self.angVel = 0
+        self.angAcc = -g / length * numpy.sin(initAngle)
+
+    def process(self):
+        self.updateAngularAcceleration()
+        self.updateAngVelocity()
+        self.updateAngle()
+
+    def updateAngularAcceleration(self): # need to update the angle every time we get called
+        self.angAcc = - self.oscillationConst * numpy.sin(self.angle)
+    def updateAngVelocity(self):
+        self.angVel += self.angAcc * frameTime
+    def updateAngle(self):
+        self.angle += self.angVel * frameTime
+    def getXCoordinate(self):
+        return self.length * numpy.cos(self.angle)
+    def getYCoordinate(self):
+        return self.length * numpy.sin(self.angle)
         
 
 def main():
@@ -27,11 +43,17 @@ def main():
     rows = 400
 
     window = GraphWin("pendulum", rows, cols, autoflush=False)
-    window.plot(200, 200, "black")
+    pendulum = Pendulum(20, 100)
+    circle = Circle(Point(200 - pendulum.getXCoordinate(), pendulum.getYCoordinate() - 200), 20)
+    circle.setFill('pink')
+    circle.draw(window)
 
-
-    window.getMouse()
-    window.close()
-
+    while True:
+        pendulum.process()
+        circle = Circle(Point(200 - pendulum.getXCoordinate(), pendulum.getYCoordinate() - 200), 20)
+        circle.undraw()
+        circle.draw(window)
+        # window.getMouse()
+        # window.close()
 
 main()

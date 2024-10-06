40206cf6f908a5aea07cae4a0031318b14fa7d38
Joey Peterson <joey.peterson15@gmail.com>
2024-10-06 13:53:36 -0700

    force arrows working and cleanup

--- index.py
+++ index.py
@@ -9,8 +9,6 @@ import numpy
     #       d^2(θ) / d(t)^2 + (g/L)sin(θ) = 0
 g = 9.8
 pi = 3.14
-xShift = 400
-yShift = 400
 scalar = 5
 
 class Pendulum:
@@ -26,7 +24,7 @@ class Pendulum:
         self._updateAngVelocity()
         self._updateAngle()
 
-    def _updateAngularAcceleration(self): # need to update the angle every time we get called
+    def _updateAngularAcceleration(self): # need to update the angle every time we get called(Eulers method)
         self.angAcc = -self.oscillationConst * numpy.sin(self.angle)
     def _updateAngVelocity(self):
         self.angVel += self.angAcc
@@ -36,80 +34,68 @@ class Pendulum:
     def getXCoordinate(self):
         return self.length * numpy.sin(self.angle)
     def getYCoordinate(self):
-        return self.length * numpy.cos(self.angle)
-# class forceArrow:
-        
+        return self.length * numpy.cos(self.angle)        
 
 def main():
     cols = 800
     rows = 800
+    xCenter = rows / 2
     window = GraphWin("pendulum", rows, cols, autoflush=False)
 
     pendulum = Pendulum(60, 120)
-    xCoor = 400 + pendulum.getXCoordinate()
+    xCoor = xCenter + pendulum.getXCoordinate()
     yCoor = pendulum.getYCoordinate()
 
     circle = Circle(Point(xCoor, yCoor), 8)
     circle.setFill('pink')
     circle.draw(window)
 
-    pointRotation = Circle(Point(400, 0), 2)
+    pointRotation = Circle(Point(xCenter, 0), 2)
     pointRotation.setFill("pink")
     pointRotation.draw(window)
-    aLine = Line(Point(400,0), Point(xCoor, yCoor))
+    aLine = Line(Point(xCenter,0), Point(xCoor, yCoor))
     aLine.draw(window)
 
-    # upArrowForce = Line(Point(xCoor, yCoor), Point(xCoor - 5 * g * numpy.sin(pendulum.angle), yCoor - 5 * g * numpy.cos(pendulum.angle)))
-    # upArrowForce.setArrow("last")
-    # upArrowForce.draw(window)
+    cosTensionForce = Line(Point(xCoor, yCoor), Point(xCoor + g * numpy.sin(pendulum.angle), yCoor + (g * numpy.cos(pendulum.angle))))
+    cosTensionForce.draw(window)
 
-    downTensionForce = Line(Point(xCoor, yCoor), Point(xCoor + g * numpy.sin(pendulum.angle), yCoor + (g * numpy.cos(pendulum.angle))))
-    downTensionForce.draw(window)
-
-    forceArrow = Line(Point(xCoor, yCoor), Point(400 + g * (numpy.cos(pendulum.angle)), g * -numpy.sin(pendulum.angle)))
-    forceArrow.draw(window)
+    sinForceArrow = Line(Point(xCoor, yCoor), Point(xCenter + g * (numpy.cos(pendulum.angle)), g * -numpy.sin(pendulum.angle)))
+    sinForceArrow.draw(window)
 
     gForceMagnitude = scalar * g
     gForce = Line(Point(xCoor, yCoor), Point(xCoor, yCoor + gForceMagnitude))
-
-
-
-
 
     while True:
         pendulum.process()
     
         circle.undraw()
         aLine.undraw()
-        forceArrow.undraw()
-        downTensionForce.undraw()
+        sinForceArrow.undraw()
+        cosTensionForce.undraw()
         gForce.undraw()
 
-        xPos = 400 + (pendulum.getXCoordinate())
+        xPos = xCenter + (pendulum.getXCoordinate())
         YPos = pendulum.getYCoordinate()
 
         circle = Circle(Point(xPos, YPos), 8)
-        aLine = Line(Point(400,0), Point(xPos, YPos))
+        aLine = Line(Point(xCenter,0), Point(xPos, YPos))
         aLine.draw(window)
         circle.setFill('pink')
         circle.draw(window)
 
-        forceArrow = Line(Point(xPos, YPos), Point(xPos - 5 * g * numpy.sin(pendulum.angle), pendulum.length))
-        # tensionScale = 5 * abs(numpy.sin(pendulum.an))
-        # forceArrow = Line(Point(xPos, YPos), Point(xPos - 5 * g * numpy.sin(pendulum.angle), ))
-        forceArrow.setArrow("last")
-        forceArrow.draw(window)
+        sinForceScalar = scalar * numpy.sin(pendulum.angle)
+        sinForceMagnitude = g * sinForceScalar
+        sinForceXDirection = -numpy.cos(pendulum.angle) #to make perpendicular, swap the left and right and change the new x negative
+        sinForceYDirection = numpy.sin(pendulum.angle)
+        sinForceArrow = Line(Point(xPos, YPos), Point(xPos + sinForceXDirection * sinForceMagnitude, YPos + sinForceYDirection * sinForceMagnitude))
+        sinForceArrow.setArrow("last")
+        sinForceArrow.draw(window)
 
-        # upArrowForce.undraw()
-        # upArrowForce  = Line(Point(xPos, YPos), Point(xPos + numpy.cos(pi / 2 + pendulum.angle) * 5 * g, YPos - numpy.sin(pi / 2 + pendulum.angle) * 5 * g))
-        # upArrowForce.setArrow("last")
-        # upArrowForce.draw(window)
+        tensionScale = 5 * abs(numpy.cos(pendulum.angle))
+        cosTensionForce = Line(Point(xPos, YPos), Point(xPos + g * tensionScale * numpy.sin(pendulum.angle), YPos + (g * tensionScale * numpy.cos(pendulum.angle))))
 
-        tensionScale = 5 * abs(numpy.cos(pendulum.angle)) ** 2
-        downTensionForce = Line(Point(xPos, YPos), Point(xPos + g * tensionScale * numpy.sin(pendulum.angle), YPos + (g * tensionScale * numpy.cos(pendulum.angle))))
-
-        downTensionForce.setArrow("last")
-        downTensionForce.draw(window)
+        cosTensionForce.setArrow("last")
+        cosTensionForce.draw(window)
 
         gForceMagnitude = scalar * g
         gForce = Line(Point(xPos, YPos), Point(xPos, YPos + gForceMagnitude))
@@ -120,6 +106,6 @@ def main():
             break
         window.update()
 
-        time.sleep(0.3)
+        time.sleep(0.13)
 
 main()
